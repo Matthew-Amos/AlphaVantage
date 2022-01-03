df5b54aa35259f0f56bff85052d3287e9fa86722
Matthew-Amos <MatthewAmos@live.com>
2022-01-03 11:21:15 -0500

    updated docstrings

--- src/av_http.jl
+++ src/av_http.jl
@@ -1,16 +1,25 @@
 """
 This submodule contains the HTTP interface to the AlphaVantage API.
 
-`AlphaVantageClient` owns the data needed to construct the GET request.
+The `AlphaVantageClient` struct owns the data needed for the HTTP requests.
 
-A `Base.get` method takes the above client, a data type parameter that inherits from
-AVFunction (see av_types.jl), and a Dict of query parameters. It executes the HTTP request
-as well as dispatches to appropriate preprocessor and postprocessor functions (see av_handlers.jl).
+A `Base.get` method takes as arguments the above client, a data type parameter that inherits from
+AVFunction (see av_types.jl), and a Dict of query parameters. It then executes the HTTP request
+as well as dispatches to appropriate pre/postprocessor functions (see av_handlers.jl).
 """
 
-# TODO: request retry
+"""
+HTTP client for connecting to AlphaVantage API.
 
-"Contains basic HTTP information for connecting to AlphaVantage API."
+```
+AlphaVantageClient(;scheme, host, key)
+```
+
+Appropriate defaults are set for `scheme` and `host` fields. You must provide an API key via a named parameter
+or by creating an ALPHA_VANTAGE_API_KEY environment variable prior to launching your Julia session. See
+https://www.alphavantage.co/support/#api-key to obtain an API key. If a key is not provided, the default is
+to use "demo" which may have restricted support.
+"""
 struct AlphaVantageClient
     scheme::String
     host::String
@@ -26,6 +35,7 @@ AlphaVantageClient(;scheme=HTTP_SCHEME, host=HTTP_HOST, key=_api_key()) = AlphaV
 _create_uri(c::AlphaVantageClient, q::D where D <: Dict{String}) = string(HTTP.URI(scheme=c.scheme, host=c.host, query=q))
 
 # `Base.get` method dispatching
+# TODO: request retry
 function Base.get(c::AlphaVantageClient, params)
     req = HTTP.request("GET", _create_uri(c, params))
     
