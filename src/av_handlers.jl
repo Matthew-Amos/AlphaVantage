96df34718c05f2d9da5d0741dbb8c38e4993c462
Matthew-Amos <MatthewAmos@live.com>
2022-01-07 23:58:48 -0500

    time series handlers

--- src/av_handlers.jl
+++ src/av_handlers.jl
@@ -13,38 +13,47 @@ The internal mechanics for handling API calls are to dispatch information to
 # Utilities
 "Ensure 'datatype' parameter is json or csv"
 function _params_datatype(d::D where D <: Dict{String})
-    dt = get(d, "datatype", "json")
-    @argcheck dt in ["json", "csv"]
-    d["datatype"] = dt
-    d
+  dt = get(d, "datatype", "json")
+  @argcheck dt in ["json", "csv"]
+  d["datatype"] = dt
+  d
 end
 
 "Overrides datatype to json"
 function _params_force_json(d::D where D <: Dict{String})
-    d["datatype"] = "json"
-    d
+  d["datatype"] = "json"
+  d
 end
 
 "Overrides datatype to csv"
 function _params_force_csv(d::D where D <: Dict{String})
-    d["datatype"] = "csv"
-    d
+  d["datatype"] = "csv"
+  d
 end
 
 "Converts response to a string"
 function _resp_tostring(resp)
-    String(resp)
+  String(resp)
 end
 
-# Handlers
+# General Handlers
 "Preprocesses API query parameters"
 function preprocess(f::A where A <: AVFunction, params=Dict{String, Any}())
-    params = _params_datatype(params)
-    params
+  params = _params_datatype(params)
+  params
 end
 
 "Postprocesses API response"
 function postprocess(f::A where A <: AVFunction, resp)
-    resp = _resp_tostring(resp)
-    resp
+  resp = _resp_tostring(resp)
+  resp
+end
+
+# Time Series Handlers
+function preprocess(f::A where A <: AVTimeSeries, params=Dict{String, Any}())
+  _params_force_csv(params)
+end
+
+function postprocess(f::A where A <: AVTimeSeries, resp)
+  CSV.File(resp) |> DataFrame
 end
