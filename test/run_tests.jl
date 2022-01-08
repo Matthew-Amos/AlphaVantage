620e10c9861e1a0388760a9a30280ff110294dc6
Matthew-Amos <MatthewAmos@live.com>
2022-01-07 22:29:23 -0500

    added Base.get test for direct request

--- test/run_tests.jl
+++ test/run_tests.jl
@@ -8,6 +8,18 @@ using AlphaVantage, Test
         c = AlphaVantageClient(;key="123")
         @test c.key == "123"
     end
+    
+    @testset "direct get" begin
+        c = AlphaVantageClient()
+        p = Dict(
+            "function" => "TIME_SERIES_MONTHLY_ADJUSTED",
+            "symbol" => "IBM",
+            "apikey" => "demo"
+        )
+        resp = get(c, p)
+        @test typeof(resp) == Vector{UInt8}
+        @test length(resp) > 0
+    end
 end
 
 @testset "handlers" begin
