261fb9922aa253027078878cc8696618c2b76feb
Matthew-Amos <MatthewAmos@live.com>
2022-01-07 23:57:36 -0500

    added defaults

--- src/av_http.jl
+++ src/av_http.jl
@@ -21,33 +21,40 @@ https://www.alphavantage.co/support/#api-key to obtain an API key. If a key is n
 to use "demo" which may have restricted support.
 """
 struct AlphaVantageClient
-    scheme::String
-    host::String
-    key::String
+  scheme::String
+  host::String
+  key::String
 end
 
 "Retrieve API key from environment, user argument, or use the default demo key."
 _api_key(k=get(ENV, ENV_VARIABLE_API_KEY, "")) = if k == "" DEFAULT_API_KEY else k end
 
 AlphaVantageClient(;scheme=HTTP_SCHEME, host=HTTP_HOST, key=_api_key()) = AlphaVantageClient(scheme, host, key)
+AlphaVantageClient(k::String) = AlphaVantageClient(;key=k)
 
 "Dynamically creates and evaluates an HTTP.URI call for a given API request."
 _create_uri(c::AlphaVantageClient, q::D where D <: Dict{String}) = string(HTTP.URI(scheme=c.scheme, host=c.host, query=q))
 
 # `Base.get` method dispatching
 function Base.get(c::AlphaVantageClient, params)
-    req = HTTP.request("GET", _create_uri(c, params))
-    
-    if req.status != 200
-        throw("HTTP request returned status code $(req.status)")
-    end
-    
-    req.body
+  params["apikey"] = c.key
+  req = HTTP.request("GET", _create_uri(c, params))
+  
+  if req.status != 200
+      throw("HTTP request returned status code $(req.status)")
+  end
+  
+  deepcopy(req.body)
 end
 
 function Base.get(c::AlphaVantageClient, f::A where A <: AVFunction, params=Dict{String, Any}())
-    params = preprocess(f, params)
-    resp = get(c, params)
-    resp = postprocess(f, resp)
-    resp
+  params["function"] = string(f)
+  params = preprocess(f, params)
+  resp = get(c, params)
+  resp = postprocess(f, resp)
+  resp
+end
+
+function Base.get(c::AlphaVantageClient, f::DataType, params=Dict{String, Any}())
+  get(c, f(), params)
 end
